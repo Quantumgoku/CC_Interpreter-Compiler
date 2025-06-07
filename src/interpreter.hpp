7aa0f385640ae5e98a5c8188decd208a1b20c368
Quantumgoku <prabhatkumatsf123@gmail.com>
2025-06-07 22:51:56 +0530

    Fix of Runtime Error

--- src/interpreter.hpp
+++ src/interpreter.hpp
@@ -6,16 +6,10 @@
 #include "Expr.hpp"
 #include "token.hpp"
 #include "Environment.hpp"
+#include "RuntimeError.hpp"
 
 class Interpreter : public ExprVisitorEval, public StmtVisitorPrint {
 public:
-
-    class RuntimeError : public std::runtime_error {
-    public:
-        Token token;
-        RuntimeError(const Token& token, const std::string& message)
-            : std::runtime_error(message), token(token) {}
-    };
 
     void visit(const Expression& stmt) const override {
         evaluate(*stmt.expression);
