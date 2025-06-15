729546375cc42fbfa31e699adc24b34ce6188f6e
Quantumgoku <prabhatkumatsf123@gmail.com>
2025-06-15 22:17:38 +0530

    Added Functions

--- src/LoxClock.cpp
+++ src/LoxClock.cpp
@@ -1,4 +1,8 @@
+#include "LoxClock.hpp"
+#include "literal.hpp"
+#include "interpreter.hpp"
 #include <ctime>
+
 lox_literal LoxClock::call(const Interpreter&, const std::vector<lox_literal>&) const {
     return static_cast<double>(std::time(nullptr));
 }
