80bdedd560de95c6bfa46118b965508c6b29dc00
Quantumgoku <prabhatkumatsf123@gmail.com>
2025-06-19 19:15:49 +0530

    Added Classes Declaration

--- src/LoxClass.hpp
+++ src/LoxClass.hpp
@@ -15,7 +15,7 @@ public:
         return name;
     }
 
-    std::string toString() const override { return "<class " + name + ">"; }
+    std::string toString() const override { return name; }
     size_t arity() const override { return 0; }
     lox_literal call(const Interpreter&, const std::vector<lox_literal>&) const override { return std::monostate{}; }
 
