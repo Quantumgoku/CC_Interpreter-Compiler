9c6eedd8a84f7de4e24e1be677a8d04019828ba5
Quantumgoku <prabhatkumatsf123@gmail.com>
2025-06-20 23:54:10 +0530

    Added Methods and Fields to instances of the classes

--- src/LoxInstance.cpp
+++ src/LoxInstance.cpp
@@ -4,7 +4,7 @@
 LoxInstance::LoxInstance(std::shared_ptr<LoxClass> klass) : klass(std::move(klass)) {}
 
 std::string LoxInstance::toString() const {
-    return klass->getName() + " instance";
+    return klass->getName();
 }
 
 lox_literal LoxInstance::call(const Interpreter&, const std::vector<lox_literal>&) {
