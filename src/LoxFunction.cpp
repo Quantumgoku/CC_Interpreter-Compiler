5639608e32d288d805aac6a23e51672b95735ab1
Quantumgoku <prabhatkumatsf123@gmail.com>
2025-06-23 23:30:01 +0530

    Testing

--- src/LoxFunction.cpp
+++ src/LoxFunction.cpp
@@ -16,6 +16,22 @@ lox_literal LoxFunction::call(const Interpreter& interpreter, const std::vector<
         }
     } catch (const ReturnException& returnValue) {
         if (isInitializer) {
+            std::cout << "[DEBUG] ReturnException in initializer, type index: " << returnValue.getValue().index();
+            if (std::holds_alternative<std::monostate>(returnValue.getValue())) {
+                std::cout << " (std::monostate)" << std::endl;
+            } else if (std::holds_alternative<double>(returnValue.getValue())) {
+                std::cout << " (double: " << std::get<double>(returnValue.getValue()) << ")" << std::endl;
+            } else if (std::holds_alternative<std::string>(returnValue.getValue())) {
+                std::cout << " (string: '" << std::get<std::string>(returnValue.getValue()) << "')" << std::endl;
+            } else if (std::holds_alternative<bool>(returnValue.getValue())) {
+                std::cout << " (bool: " << std::get<bool>(returnValue.getValue()) << ")" << std::endl;
+            } else if (std::holds_alternative<std::shared_ptr<LoxCallable>>(returnValue.getValue())) {
+                std::cout << " (LoxCallable ptr)" << std::endl;
+            } else if (std::holds_alternative<std::shared_ptr<LoxInstance>>(returnValue.getValue())) {
+                std::cout << " (LoxInstance ptr)" << std::endl;
+            } else {
+                std::cout << " (unknown type)" << std::endl;
+            }
             if (!std::holds_alternative<std::monostate>(returnValue.getValue())) {
                 throw RuntimeError(Token(TokenType::IDENTIFIER, "return", std::monostate{}, 0), "Can't return a value from an initializer.");
             }
