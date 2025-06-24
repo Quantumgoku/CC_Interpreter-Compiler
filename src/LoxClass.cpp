a6277bafc98086ebf75213d8ac14c9de3844a55c
Quantumgoku <prabhatkumatsf123@gmail.com>
2025-06-24 22:15:00 +0530

    Test

--- src/LoxClass.cpp
+++ src/LoxClass.cpp
@@ -4,8 +4,11 @@
 lox_literal LoxClass::call(Interpreter& interpreter, const std::vector<lox_literal>& arguments) {
     auto instance = LoxInstance::create(std::static_pointer_cast<LoxClass>(shared_from_this()));
     auto initializer = findMethod("init");
-    if(initializer != nullptr) {
-        initializer->bind(instance)->call(interpreter, arguments);
+    if (initializer) {
+        auto initFunc = std::dynamic_pointer_cast<LoxFunction>(initializer);
+        if (initFunc) {
+            initFunc->bind(instance)->call(interpreter, arguments);
+        }
     }
     return instance;
 }
