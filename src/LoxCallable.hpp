1b5da4c9a3f713b24ec6c627679e5bc75642f1aa
Quantumgoku <prabhatkumatsf123@gmail.com>
2025-06-24 21:08:58 +0530

    Test

--- src/LoxCallable.hpp
+++ src/LoxCallable.hpp
@@ -10,6 +10,6 @@ class LoxCallable {
 public:
     virtual ~LoxCallable() {} // Ensure proper cleanup of derived classes
     virtual size_t arity() const = 0;
-    virtual lox_literal call(const Interpreter& interpreter, const std::vector<lox_literal>& arguments) = 0;
+    virtual lox_literal call(Interpreter& interpreter, const std::vector<lox_literal>& arguments) = 0;
     virtual std::string toString() const = 0;
 };
